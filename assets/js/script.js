f768aa826d723c5afa02d26996aee1dcd3870980
Jyoti Hambir <jyotiqa2022@gmail.com>
2025-03-24 14:51:55 +0000

    add toggle bar for nav for media screen 1176px

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+let section = document.querySelector('section');
+
+menuToggle.onclick = function(){
+    header.classList.toggle('active')
+    section.classList.toggle('active')
+}
